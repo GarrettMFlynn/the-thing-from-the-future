ef500f8621e5e65c9c2d53f492ff6230970a6a9d
Garrett <gflynn@usc.edu>
2020-10-18 21:26:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# the-thing-from-the-future
+ A web version of The Thing from the Future created by Jeff Watson and the Situation Lab
---
+++ js/site.js
@@ -0,0 +1,74 @@
+
+function allQueries() {
+    let all = ['future','thing','theme']
+    for (const ind in all) {
+        queryCSV(all[ind])
+    }
+}
+
+function queryCSV(text_id) {
+    let file_id = ''
+
+    let file = 'assets/cards.csv'
+    d3.csv(file).then(function (data) {
+        let flag = true;
+        let inner_flag;
+        let row;
+        let str = text_id.split('-');
+        let output;
+        let freq;
+
+
+            // Create bag of words (to account for frequency)
+            let bag = [];
+            let data_;
+            let word;
+            let components;
+            for (const r in data) {
+                data_ = data[r][str[0]]
+                if (data_ != undefined && data_ != []) {
+                    if (data_.split(' (').length == 2) {
+                        components = data_.split(' (')
+                        word = components[0];
+                        freq = components[1].split(')')[0]
+                        for (let i = 0; i < freq; i++) {
+                            bag.push(word)
+                        }
+                    } else{
+                        bag.push(data_)
+                    }
+                }
+            }
+
+            while (flag) {
+                row = Math.floor(Math.random() * bag.length);
+                output = bag[row]
+                if (output != '') {
+                    flag = false;
+                    if (output == 'a D.I.Y. Kit') {
+                        document.getElementById(text_id).innerHTML = 'a D.I.Y kit';
+                    } else{
+                        document.getElementById(text_id).innerHTML = output.toLowerCase();
+                    }
+                }
+            }
+        // updatePrompt();
+    })
+}
+
+// function updatePrompt() {
+//     let all = ['future', 'thing', 'theme']
+//     let text = [];
+//     text.push(document.getElementById('future1').innerHTML)
+//     text.push(document.getElementById('future').innerHTML.toLowerCase())
+//     text.push(document.getElementById('future2').innerHTML.toLowerCase() + ',')
+//     text.push(document.getElementById('thing1').innerHTML.toLowerCase())
+//     text.push(document.getElementById('thing').innerHTML.toLowerCase())
+//     text.push(document.getElementById('theme1').innerHTML.toLowerCase())
+//     text.push(document.getElementById('theme').innerHTML.toLowerCase() + '.')
+//     text.push(document.getElementById('theme2').innerHTML)
+//
+//
+//     let prompt = text.join(' ')
+//     document.getElementById('Prompt').innerHTML = prompt
+// }
